0f28dafa4970e7ff265adfe7ce71502a1eec606d
OMKAR GAIKWAD <gaikwadomkar460@gmail.com>
2024-12-06 01:17:54 +0530

    Code Uploaded

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3001;
+
+app.get('/') , (req,res) => {
+    res.send("Hello World");
+}
+
+app.listen(3001 , () => {
+    console.log(`Server started running on port ${port}`)
+});
